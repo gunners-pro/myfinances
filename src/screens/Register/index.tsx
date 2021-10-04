4f766eb900c9a24685905e2104a776c8039855e8
Fabricyo Barreto <bass.barreto@gmail.com>
2021-10-04 19:55:23 -0300

    Component/CategorySelect: It was made the initial config of the component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Button } from '../../components/Forms/Button'
+import { CategorySelect } from '../../components/Forms/CategorySelect'
 import { Input } from '../../components/Forms/Input'
 import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton'
 import { Container, Header, Title, Form, Fields, TransactionsTypes } from './styles'
@@ -35,6 +36,8 @@ export function Register() {
               isActive={transactionType === 'down'}
             />
           </TransactionsTypes>
+
+          <CategorySelect title="Categoria" />
         </Fields>
 
         <Button title="Enviar" />
