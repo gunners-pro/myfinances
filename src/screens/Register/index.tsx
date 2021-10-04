34ccbe1cdb858a70a0749d0e94ea232320270b62
Fabricyo Barreto <bass.barreto@gmail.com>
2021-10-04 13:24:22 -0300

    Screen/Register: initial config

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { Container, Header, Title } from './styles'
+
+export function Register() {
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+    </Container>
+  )
+}
