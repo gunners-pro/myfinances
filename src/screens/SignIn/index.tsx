c5d8e632d95d7287498f42d078b833fca78b283e
Fabricyo Barreto <fabriciobarreto@hotmail.com>
2022-01-16 10:03:26 -0300

    SignIn: add animation to buttons login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, Alert, Platform, Dimensions } from 'react-native';
+import { ActivityIndicator, Alert, Dimensions } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useTheme } from 'styled-components';
-import {
+import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
-import AppleSvg from '../../assets/apple.svg';
 import GoogleSvg from '../../assets/google.svg';
 import FacebookSvg from '../../assets/facebook.svg';
 import LogoSvg from '../../assets/logo.svg';
@@ -24,20 +23,32 @@ import {
 } from './styles';
 import { SignInFacebookSocialButton } from '../../components/SignInFacebookSocialButton';
 
-const { height } = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 
 export function SignIn() {
   const { signInWithGoogle, signInWithFacebook } = useAuth();
   const [isLoading, setIsLoading] = useState(false);
   const theme = useTheme();
   const translateY = useSharedValue(-height / 1.5);
-  const tStyle = useAnimatedStyle(() => ({
+  const translateYStyle = useAnimatedStyle(() => ({
     transform: [{ translateY: translateY.value }],
+  }));
+  const translateXBtnGoogle = useSharedValue(-width);
+  const translateXBtnGoogleStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: translateXBtnGoogle.value }],
+  }));
+  const translateXBtnFacebook = useSharedValue(width);
+  const translateXBtnFacebookStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: translateXBtnFacebook.value }],
   }));
 
   useEffect(() => {
-    translateY.value = withTiming(0, { duration: 500 });
-  }, [translateY]);
+    translateY.value = withTiming(0, { duration: 400 }, () => {
+      translateXBtnGoogle.value = withTiming(0, { duration: 250 }, () => {
+        translateXBtnFacebook.value = withTiming(0, { duration: 250 });
+      });
+    });
+  }, [translateY, translateXBtnGoogle, translateXBtnFacebook]);
 
   function handleSignInGoogle() {
     try {
@@ -61,7 +72,7 @@ export function SignIn() {
 
   return (
     <Container>
-      <Header style={tStyle}>
+      <Header style={translateYStyle}>
         <TitleWrapper>
           <LogoSvg width={RFValue(140)} height={RFValue(140)} />
           <Title>
@@ -76,19 +87,20 @@ export function SignIn() {
 
       <Footer>
         <FooterWrapper>
-          <SignInSocialButton
-            onPress={handleSignInGoogle}
-            title="Entrar com Google"
-            svg={GoogleSvg}
-          />
-          {Platform.OS === 'ios' && (
-            <SignInSocialButton title="Entrar com Apple" svg={AppleSvg} />
-          )}
-          <SignInFacebookSocialButton
-            onPress={handleSignInFacebook}
-            title="Entrar com Facebook"
-            svg={FacebookSvg}
-          />
+          <Animated.View style={translateXBtnGoogleStyle}>
+            <SignInSocialButton
+              onPress={handleSignInGoogle}
+              title="Entrar com Google"
+              svg={GoogleSvg}
+            />
+          </Animated.View>
+          <Animated.View style={translateXBtnFacebookStyle}>
+            <SignInFacebookSocialButton
+              onPress={handleSignInFacebook}
+              title="Entrar com Facebook"
+              svg={FacebookSvg}
+            />
+          </Animated.View>
         </FooterWrapper>
         {isLoading && (
           <ActivityIndicator
